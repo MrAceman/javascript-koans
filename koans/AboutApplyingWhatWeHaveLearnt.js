86c109e2cc80988c5083bd5696bc494aa9686b15
Niclas Willems <niclas.willems@gmail.com>
2016-07-11 12:19:37 -0400

    Missing some final functions.  Will return to this when I can.

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -32,13 +32,14 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
 
       var productsICanEat = [];
 
+      // products.filter(any )
       /* solve using filter() & all() / any() */
 
       expect(productsICanEat.length).toBe(FILL_ME_IN);
@@ -55,7 +56,7 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
 
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
